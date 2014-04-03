fe10ffa3b07a5eee425612abbf14af91004df3c6
tashiro_h <suka.sukatan@gmail.com>
2014-04-03 19:32:34 +0900

    test

---
+++ SampleApp/SampleApp/AppDelegate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace SampleApp
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
+        SampleAppViewController viewController;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+			
+            viewController = new SampleAppViewController();
+            window.RootViewController = viewController;
+            window.MakeKeyAndVisible();
+			
+            return true;
+        }
+    }
+}
+
---
+++ SampleApp/SampleApp/SampleAppViewController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace SampleApp
+{
+    public partial class SampleAppViewController : UIViewController
+    {
+        public SampleAppViewController() : base("SampleAppViewController", null)
+        {
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+    }
+}
+
